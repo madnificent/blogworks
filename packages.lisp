9a1cd5270b155b6599678789d52ae232e3ebe7bc
MADnificent <madnificent@gmail.com>
2009-04-28 13:07:08 +0200

    Defining load order of files, as the model needs to be loaded first (macrology)

--- packages.lisp
+++ packages.lisp
@@ -18,7 +18,7 @@
   (:export :create-new-blog :find-blog-by-id
 	   :get-latest-blogs)
   (:export :create-new-post :get-latest-posts :get-posts-for-blog
-	   :find-post-by-id)
+	   :find-post-by-id :update-modified-post)
   (:export :create-new-comment :get-comments-for-post))
 
 (defpackage :blogworks.site
@@ -27,7 +27,7 @@
 	:claymore
 	:claymore.html)
   (:shadowing-import-from :blogworks.model :title)
-  (:export :welcome :new-user :create-user :login :personal-user-page :public-user-page :logout :blog-page :post-page :new-blog :create-blog :new-post :create-post :create-comment)
+  (:export :welcome :new-user :create-user :login :personal-user-page :public-user-page :logout :blog-page :post-page :new-blog :create-blog :new-post :create-post :create-comment :edit-post :update-post)
   (:export :nickname :password :email :logged-in-user :redirect-to-welcome :user :blog :post))
 
 (defpackage :blogworks.routing
